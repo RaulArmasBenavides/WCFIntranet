a2ef8b5c087645327b4040a6af73d18c9e02f00d
Raúl Armas <raularmasbx@gmail.com>
2024-05-01 11:01:52 -0500

    updates

---
+++ intranet.dataaccess/DataBase/AccesoDB.cs
@@ -0,0 +1,16 @@
+﻿
+using intranet.service;
+using System.Configuration;
+
+
+namespace intranet.dataaccess.DataBase
+{
+    public class AccesoDB  
+    {   
+        public AccesoDB()
+        {
+        }
+
+    
+    }
+}
